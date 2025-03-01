80e0f0eebcd69b0a332aedd0be25026281044589
tyraf <tyrafeide@gmail.com>
2025-03-01 15:13:27 +0100

    Added functionality to package project as war file.

--- backend/Gallery/src/main/java/com/Gallery/GalleryApplication.java
+++ backend/Gallery/src/main/java/com/Gallery/GalleryApplication.java
@@ -2,9 +2,10 @@ package com.Gallery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GalleryApplication {
+public class GalleryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GalleryApplication.class, args);
