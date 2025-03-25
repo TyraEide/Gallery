f1f9a8f85971e6c52d7c116adefbb9ab1103bc8d
tyraf <tyrafeide@gmail.com>
2025-03-25 13:17:19 +0100

    CourseService getAnnouncement implemented and tested.

--- backend/Gallery/src/main/java/com/Gallery/model/DiscussionTopic.java
+++ backend/Gallery/src/main/java/com/Gallery/model/DiscussionTopic.java
@@ -135,5 +135,17 @@ public class DiscussionTopic {
         this.delayed_post_at = delayed_post_at;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
 
+        DiscussionTopic other = (DiscussionTopic) obj;
+
+        return this.getUrl().equals(other.getUrl());
+    }
 }
--- backend/Gallery/src/main/java/com/Gallery/service/CourseService.java
+++ backend/Gallery/src/main/java/com/Gallery/service/CourseService.java
@@ -2,24 +2,63 @@ package com.Gallery.service;
 
 import com.Gallery.model.DiscussionTopic;
 import com.Gallery.model.User;
-import org.springframework.http.HttpStatus;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Service
 public class CourseService {
+    private final RestTemplate restTemplate;
+    private final String uibApiUrlBase = "https://mitt.uib.no/api/v1";
+    private final String hvlApiUrlBase = "https://hvl.instructure.com/api/v1";
 
-    private final WebClient webClient;
-
-    public CourseService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("").build();
+    public CourseService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    public List<DiscussionTopic> getAnnouncements(String institution,String course_id, User user) {
+    public DiscussionTopic[] getAnnouncements(String institution, String[] courseIds, User user) {
+        String token = getToken(institution, user);
+        if(token == null) {
+            throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED, "User has not set " + institution + " authorization token.");
+        }
+
+        String baseApiUrl = getBaseApiUrl(institution);
+        String apiUrl = baseApiUrl + "/announcements?context_codes[]={validCourseIds}";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.set("Authorization", "Bearer " + token);
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<DiscussionTopic[]> response = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.GET,
+                request,
+                DiscussionTopic[].class,
+                Arrays.toString(courseIds));
+
+        return response.getBody();
+    }
+
+    private String getBaseApiUrl(String institution) {
+        if(institution.equals("uib")) {
+            return uibApiUrlBase;
+        }
+        else if(institution.equals("hvl")) {
+            return hvlApiUrlBase;
+        }
+        else {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "No such institution found: " + institution);
+        }
+    }
+
+    private String getToken(String institution, User user) {
         String token;
         if(institution.equals("uib")) {
             token = user.getUibToken();
@@ -28,13 +67,8 @@ public class CourseService {
             token = user.getHvlToken();
         }
         else {
-            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "No such institution found: " + institution);
         }
-
-        if(token == null) {
-            throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
-        }
-
-
+        return token;
     }
 }
