a89dbfafbba4bbd567a0351cec9bb33dddade189
Philip.Svenningsen <psv007@uib.no>
2025-03-26 14:49:49 +0100

    improved login, incl. JWT token

--- Frontend/DAT251-frontend/src/pages/Login.svelte
+++ Frontend/DAT251-frontend/src/pages/Login.svelte
@@ -1,84 +1,127 @@
 <script lang="ts">
 
-  import {api_url} from "../ts_modules/api"
+  import {api_url,set_jwt_token,jwt_token_header} from "../ts_modules/api"
   import {redirect } from "../ts_modules/routing"
 
   console.log("page loaded")
 
-  // General notice ⚠️
-  // Session ID is not set up correctly
-  // Should be set with set_cookie header / HTTPS
-  // Causes error when host domain == client domain
+  // TODO
+  // CSRF implementation ?
 
-  // General notice ⚠️
-  // Application should be modified to communicate with back end instead of python flask test server
-
-  let username:       string = ""
+  let email:          string = ""
   let password:       string = ""
-  let cookie_val:     string = ""
-  let response_msg:   string = ""
 
-  function setCookie(cookie_name:string,val:string){
-        // ⚠️ WARNING : This cookie will not be sent with other requests due to misaligned domain
-        // Session ID should be set with the set_cookie header. Use HTTPS 
-        document.cookie = cookie_name + " = " + val
-        
-  }
-  
-  // Default login request to specified URL
-  async function login_request(url:string){
-    // request
-    const response_json = await fetch(url, {method: "POST", credentials: "include", headers: {
-        'content-type': 'application/json'
-        }, body: JSON.stringify({
-                username: username,
-                password: password
-            })
-    }).then((r) => r.json())
+  let message:        string = "-"
+  let cached_email:   string = ""
 
-    // output value
-    response_msg = response_json.message
+  let loading:        boolean = false
 
-    // set sessionID cookie from JSON
-    if(response_json.sessionID != undefined){
-      setCookie("SessionID",response_json.sessionID)
-      redirect("dashboard")
+  // Login request
+  async function login_request(){
+    if(cached_email == ""){
+
+      cached_email = email // to avoid concurrency errors // working as lock too
+      message = "Waiting for response"
+
+      let request_failed = false
+      
+      // request
+      const response_json = await fetch(api_url("/login"), 
+        {method: "POST", 
+        signal : AbortSignal.timeout(5000),
+        headers: {
+          'content-type': 'application/json'
+          }, body: JSON.stringify({
+                  email: email,
+                  password: password
+              })
+      }).then((r) => r.json()).catch(() => request_failed = true)
+
+      if(request_failed){
+        cached_email = ""
+        message = "Failed to reach server"
+        return
+      }
+
+      // output value
+      message = response_json.message
+
+      // set sessionID cookie from JSON
+      if(response_json.token != undefined && response_json.user != undefined){
+        if(response_json.user.email == cached_email){
+          set_jwt_token(response_json.token)
+          setTimeout(() => redirect("dashboard"), 600)
+        }
+      }
+      else{
+        console.log("malformed response")
+      }
+
+      cached_email = ""
     }
     else{
-      console.log("Missing Cookie")
+      console.log("Already waiting for response")
     }
     
   }
 
-  // Debug function for "localhost:5000"
-  async function check_message_cookie_req(){
-    const response_json = await fetch(api_url("/cookie"), {credentials: "include"}).then((r) => r.json())
-    response_msg = response_json.message
+  async function jwt_sample_req(){
+    const headers = jwt_token_header();
+    const response_json = await fetch(api_url("/jwt"),{headers}).then((r) => r.json())
+    message = response_json.message
   }
 
 </script>
 
 <main>
   <div>
-    <h2>Login Section</h2>
-    <div>
-      Username: <input type="text" bind:value={username}><br>
-      Password: <input type="text" bind:value={password}><br>
-      <input type="submit" onclick={() => {login_request(api_url("/login"))}}>
-    </div>
-    <div>
-      Output: {response_msg}
-    </div>
 
-    <div>
-      <button type="button" onclick={() => {redirect("register")}}>Register account</button>
-    </div>
+    <h2>Login</h2>
+    <form onsubmit={() => login_request()}>
+
+        <label for="email">Email</label>
+        <input
+            id="email"
+            type="text"
+            bind:value={email}
+            required
+            autocomplete="email"
+        />
+
+        <label for="password">Password</label>
+        <input
+            id="password"
+            type="password"
+            bind:value={password}
+            required
+            autocomplete="current-password"
+        />
+
+        <button type="submit" disabled={loading}> 
+            {#if loading}
+                <span class="spinner"></span> Loading...
+            {:else}
+                Login
+            {/if}
+        </button>
+
+        
+    </form>
+    
+    <p>{message}</p>
+
+    <hr>
+
+    Sign up instead?
+    <p><button type="button" onclick={() => {redirect("register")}}>Register account</button></p>
   </div>
+
+  <hr>
 
 
   <div>
     <h2>Debug Section</h2>
-    <button onclick={() => {check_message_cookie_req()}}>Check if cookie?</button>
+    <button onclick={() => {jwt_sample_req()}}>Check valid JWT?</button>
   </div>
 
   <a href="https://vite.dev">Vite docs</a><br>
--- Frontend/DAT251-frontend/src/ts_modules/api.ts
+++ Frontend/DAT251-frontend/src/ts_modules/api.ts
@@ -3,3 +3,14 @@ const base_url = import.meta.env.VITE_API_URL;
 export function api_url(path:string){
     return base_url + path;
 }
+
+export function set_jwt_token(token:string){
+    localStorage.setItem("token",token)
+}
+
+export function jwt_token_header(){
+    const token     = localStorage.getItem("token")
+    const headers   = new Headers({})
+    headers.set("Authorization", "Bearer " + token)
+    return headers
+}
