75d8718414204d5e56fc09b5c08595d905d38e46
Philip.Svenningsen <psv007@uib.no>
2025-03-26 15:00:28 +0100

    code cleanup

--- Frontend/DAT251-frontend/src/pages/Login.svelte
+++ Frontend/DAT251-frontend/src/pages/Login.svelte
@@ -11,58 +11,50 @@
   let email:          string = ""
   let password:       string = ""
 
-  let message:        string = "-"
+  let message:        string = "..."
   let cached_email:   string = ""
 
-  let loading:        boolean = false
 
   // Login request
   async function login_request(){
-    if(cached_email == ""){
+    cached_email = email // to avoid concurrency errors // working as lock too
 
-      cached_email = email // to avoid concurrency errors // working as lock too
-      message = "Waiting for response"
-
-      let request_failed = false
+    let request_failed = false
       
-      // request
-      const response_json = await fetch(api_url("/login"), 
-        {method: "POST", 
-        signal : AbortSignal.timeout(5000),
-        headers: {
-          'content-type': 'application/json'
-          }, body: JSON.stringify({
-                  email: email,
-                  password: password
-              })
-      }).then((r) => r.json()).catch(() => request_failed = true)
+    // request
+    const response_json = await fetch(api_url("/login"), 
+      {method: "POST", 
+      signal : AbortSignal.timeout(5000),
+      headers: {
+        'content-type': 'application/json'
+        }, body: JSON.stringify({
+                email: email,
+                password: password
+            })
+    }).then((r) => r.json()).catch(() => request_failed = true)
 
-      if(request_failed){
-        cached_email = ""
-        message = "Failed to reach server"
-        return
-      }
-
-      // output value
-      message = response_json.message
-
-      // set sessionID cookie from JSON
-      if(response_json.token != undefined && response_json.user != undefined){
-        if(response_json.user.email == cached_email){
-          set_jwt_token(response_json.token)
-          setTimeout(() => redirect("dashboard"), 600)
-        }
-      }
-      else{
-        console.log("malformed response")
-      }
-
+    // check if response was reccived
+    if(request_failed){
       cached_email = ""
+      message = "Failed to reach server"
+      return
+    }
+
+    // output value
+    message = response_json.message
+
+    // set JWT from json
+    if(response_json.token != undefined && response_json.user != undefined){
+      if(response_json.user.email == cached_email){
+        set_jwt_token(response_json.token)
+        setTimeout(() => redirect("dashboard"), 600)
+      }
     }
     else{
-      console.log("Already waiting for response")
+      console.log("malformed response")
     }
-    
+
+    cached_email = ""
   }
 
   async function jwt_sample_req(){
@@ -97,8 +89,8 @@
             autocomplete="current-password"
         />
 
-        <button type="submit" disabled={loading}> 
-            {#if loading}
+        <button type="submit" disabled={cached_email != ""}> 
+            {#if cached_email != ""}
                 <span class="spinner"></span> Loading...
             {:else}
                 Login
@@ -115,17 +107,6 @@
     Sign up instead?
     <p><button type="button" onclick={() => {redirect("register")}}>Register account</button></p>
   </div>
-
-  <hr>
-
-
-  <div>
-    <h2>Debug Section</h2>
-    <button onclick={() => {jwt_sample_req()}}>Check valid JWT?</button>
-  </div>
-
-  <a href="https://vite.dev">Vite docs</a><br>
-  <a href="https://svelte.dev">Svelte docs</a>
 </main>
  
 <style>
