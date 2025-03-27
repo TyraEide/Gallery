ac997bb1dee6e6256bf07e34f14f0bc49eefd048
Philip.Svenningsen <psv007@uib.no>
2025-03-27 13:50:21 +0100

    testing & small fixes to login

--- Frontend/DAT251-frontend/src/pages/Login.svelte
+++ Frontend/DAT251-frontend/src/pages/Login.svelte
@@ -16,47 +16,68 @@
 
 
   // Login request
-  async function login_request(){
+  async function login_request(event:Event){
+
+    event.preventDefault()
+
     cached_email = email // to avoid concurrency errors // working as lock too
 
-    let request_failed = false
-      
-    // request
-    const response_json = await fetch(api_url("/login"), 
-      {method: "POST", 
-      signal : AbortSignal.timeout(5000),
-      headers: {
-        'content-type': 'application/json'
-        }, body: JSON.stringify({
-                email: email,
-                password: password
-            })
-    }).then((r) => r.json()).catch(() => request_failed = true)
+    try{
 
-    // check if response was reccived
-    if(request_failed){
-      cached_email = ""
-      message = "Failed to reach server"
-      return
-    }
+      // request
+      const response = await fetch(api_url("/login"), 
+        {method: "POST", 
+        signal : AbortSignal.timeout(8000),
+        headers: {
+          'content-type': 'application/json'
+          }, body: JSON.stringify({
+                  email: email,
+                  password: password
+              })
+      })
 
-    // output value
-    message = response_json.message
+      if(!response.ok){
+        switch(response.status){
+          case 401: 
+            message = "Incorrect email or password"
+            break
 
-    // set JWT from json
-    if(response_json.token != undefined && response_json.user != undefined){
-      if(response_json.user.email == cached_email){
-        set_jwt_token(response_json.token)
-        setTimeout(() => redirect("dashboard"), 600)
+          default:
+            message = "unknown error (status error)"
+            console.log("response status code:" + response.status)
+            break
+
+        }
       }
-    }
-    else{
-      console.log("malformed response")
-    }
+      else{
+
+        const response_json = await response.json()
+
+        //check for contents
+        if(response_json.token != undefined && response_json.user != undefined){
+          if(response_json.user.email == cached_email){
+            set_jwt_token(response_json.token)
+            message = "Login Successful"
+            setTimeout(() => redirect("dashboard"), 600)
+          }
+          else{
+            message = "Internal response error"
+          }
+        }
+        else{
+          message = response_json.token
+          console.log("malformed response error")
+        }
+      }
+    } 
+    catch(err){
+      message = "Issue reaching server | " + (err)
+    } 
 
     cached_email = ""
   }
 
+  // For debugging purpouses
   async function jwt_sample_req(){
     const headers = jwt_token_header();
     const response_json = await fetch(api_url("/jwt"),{headers}).then((r) => r.json())
@@ -69,7 +90,7 @@
   <div>
 
     <h2>Login</h2>
-    <form onsubmit={() => login_request()}>
+    <form onsubmit={login_request}>
 
         <label for="email">Email</label>
         <input
@@ -89,7 +110,7 @@
             autocomplete="current-password"
         />
 
-        <button type="submit" disabled={cached_email != ""}> 
+        <button type="submit" disabled={cached_email != ""}>
             {#if cached_email != ""}
                 <span class="spinner"></span> Loading...
             {:else}
