37f5779aa914cb45ba3d62ec7ff982a2a298e4e0
tyraf <tyrafeide@gmail.com>
2025-03-27 11:36:48 +0100

    Fixed merge conflicts.

--- backend/Gallery/src/main/java/com/Gallery/controller/UserController.java
+++ backend/Gallery/src/main/java/com/Gallery/controller/UserController.java
@@ -4,6 +4,7 @@ import com.Gallery.dto.UserRegistrationDTO;
 import com.Gallery.model.User;
 import com.Gallery.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
