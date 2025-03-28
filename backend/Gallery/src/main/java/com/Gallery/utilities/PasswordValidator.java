0656218b016cd68ab385350b34d06fa0ccd529a4
Thorbjorn M <thomelheim@gmail.com>
2025-03-28 14:22:22 +0100

    Moved validators to UserRegistrationDTO

--- backend/Gallery/src/main/java/com/Gallery/utilities/PasswordValidator.java
+++
@@ -1,9 +0,0 @@
-package com.Gallery.utilities;
-
-public class PasswordValidator {
-
-    private static final String regexPattern = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=]).{8,}$";
-    public static boolean validatePassword(String password) {
-        return password.matches(regexPattern);
-    }
-}
