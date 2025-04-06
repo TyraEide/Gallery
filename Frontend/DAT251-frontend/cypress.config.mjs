cf3f37e81a358986986cf83fda191a1b0799289f
Martin.Eide <43970264+mrtineide@users.noreply.github.com>
2025-04-06 17:42:17 +0200

    lets go again

--- Frontend/DAT251-frontend/cypress.config.mjs
+++ Frontend/DAT251-frontend/cypress.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
   e2e: {
     // Use the .env file to get the SPA URL of where we "host" the svelte frontend app
     baseUrl: process.env.CYPRESS_WEB_SERVER_OF_SPA || "http://localhost:5173",
+    env: {
+      API_BASE_URL: process.env.VITE_API_BASE_URL || "http://localhost:8080",
+    },
     supportFile : false, 
   },
 });
