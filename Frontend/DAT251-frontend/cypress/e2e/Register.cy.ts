c90c641c520cbf100588e3bc6bf16286d222a754
Martin.Eide <43970264+mrtineide@users.noreply.github.com>
2025-04-06 14:24:11 +0200

    Debugging the CI again...

--- Frontend/DAT251-frontend/cypress/e2e/Register.cy.ts
+++ Frontend/DAT251-frontend/cypress/e2e/Register.cy.ts
@@ -7,13 +7,15 @@ describe('User Registration', () => {
 
   it('Registers a user successfully', () => {
     // Intercept any request to the users endpoint, without specifying full URL
-    cy.intercept('POST', `/api/users`).as('registerUser');
+    cy.log(Cypress.env("baseUrl"))
+    cy.intercept('POST', "http://something/api/users").as('registerUser');
     const randomness = Math.random().toString();
     cy.get('input[id="username"]').type('testuser1'+randomness);
     cy.get('input[id="email"]').type('test1'+randomness+'@example.com');
     cy.get('input[id="password"]').type('SecurePass123!');
     cy.get('input[id="confirmPassword"]').type('SecurePass123!');
     cy.get('button[type="submit"]').click();
+    cy.log(Cypress.env("baseUrl"))
 
     cy.wait('@registerUser');
     cy.contains('Registration successful!', { timeout: 20000 }).should('be.visible');
