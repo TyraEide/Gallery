9168a5d4960e41d9f8bed0f3a1f938c9c70cd2de
tyraf <tyrafeide@gmail.com>
2025-04-08 20:43:48 +0200

    Added annotations to TokenService and TokenRepository.

--- backend/Gallery/src/main/java/com/Gallery/repository/TokenRepository.java
+++ backend/Gallery/src/main/java/com/Gallery/repository/TokenRepository.java
@@ -4,10 +4,12 @@ import com.Gallery.model.CanvasToken;
 import com.Gallery.model.CanvasTokenId;
 import com.Gallery.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TokenRepository extends JpaRepository<CanvasToken, CanvasTokenId> {
     Optional<List<CanvasToken>> getAllTokensForUser(User user);
 
