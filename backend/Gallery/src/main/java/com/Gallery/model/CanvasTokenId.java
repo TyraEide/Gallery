e64abe5207552ad6519f4a2be775699489a9fe2b
tyraf <tyrafeide@gmail.com>
2025-04-08 18:05:22 +0200

    Implemented CanvasToken entity and composite primary key CanvasTokenId. Fixed name of table for User entity as well.

---
+++ backend/Gallery/src/main/java/com/Gallery/model/CanvasTokenId.java
@@ -0,0 +1,34 @@
+package com.Gallery.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class CanvasTokenId implements Serializable {
+    private User user;
+
+    private Institution institution;
+
+    public CanvasTokenId() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Institution getInstitution() {
+        return institution;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CanvasTokenId that = (CanvasTokenId) o;
+        return Objects.equals(user, that.user) && Objects.equals(institution, that.institution);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, institution);
+    }
+}
