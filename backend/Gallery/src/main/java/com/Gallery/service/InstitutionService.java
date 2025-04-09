3f42c6ee1daa54a09d8f618d980bc2df7b36d6bd
tyraf <tyrafeide@gmail.com>
2025-04-09 14:05:00 +0200

    Implemented delete and create methods in InstitutionService.

--- backend/Gallery/src/main/java/com/Gallery/service/InstitutionService.java
+++ backend/Gallery/src/main/java/com/Gallery/service/InstitutionService.java
@@ -27,4 +27,16 @@ public class InstitutionService {
     public Optional<Institution> findByShortName(String shortName) {
         return institutionRepository.findById(shortName);
     }
+
+    public void deleteAll() {
+        institutionRepository.deleteAll();
+    }
+
+    public Institution createInstitution(Institution institution) {
+        return institutionRepository.save(institution);
+    }
+
+    public List<Institution> createAllInstitutions(List<Institution> institutions) {
+        return institutionRepository.saveAll(institutions);
+    }
 }
--- backend/Gallery/src/test/java/com/Gallery/unit/controller/CourseControllerUnitTest.java
+++ backend/Gallery/src/test/java/com/Gallery/unit/controller/CourseControllerUnitTest.java
@@ -86,8 +86,8 @@ public class CourseControllerUnitTest {
         // Prep arguments
         List<String> validCourseIds = List.of("Course1, Course2");
         User authorizedUser = new User();
-        authorizedUser.setUibToken("validToken");
-        authorizedUser.setHvlToken("validToken");
+//        authorizedUser.setUibToken("validToken");
+//        authorizedUser.setHvlToken("validToken");
 
         // Get announcements
         when(courseService.getAllAnnouncements(authorizedUser)).thenReturn(mockedApi);
