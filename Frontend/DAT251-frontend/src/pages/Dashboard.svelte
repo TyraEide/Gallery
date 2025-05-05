8b7f81340bc1df77cc802f6f6e3470469fa696eb
tyraf <tyrafeide@gmail.com>
2025-05-05 12:08:23 +0200

    Annoucnements show up on Dashboard instead of button with redirect. Loading animation when waiting for response from backend.

--- Frontend/DAT251-frontend/src/pages/Dashboard.svelte
+++ Frontend/DAT251-frontend/src/pages/Dashboard.svelte
@@ -34,6 +34,7 @@
 <main>
   <div class="header">
     <h3>{new Date().toLocaleString("default",{weekday:"long",day:"numeric",month:"long",year:"numeric"})}</h3>
+    <button on:click={() => redirect("announcements")}>Announcements</button>
   </div>
   <section class="announcement-container">
     <Announcement />
